b9902a1d8550916e1be90c385f1e5a7976638512
Garrett <gmrybarczyk@gmail.com>
2019-02-20 09:09:34 -0700

    makeDataset now checks to see if keys are there in the json before trying to access it

--- makeDataset.py
+++ makeDataset.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import os 
 import cv2
 import json
+from random import shuffle
 
 #change directory to the data to work on your machine
 DATADIR = "E:/\school/tensorflowprc/moleDataSet/Data/Images/"
@@ -18,7 +19,7 @@ DESCDIR = "E:/\school/tensorflowprc/moleDataSet/Data/Descriptions/"
 CATEGORIES = ['Benign','Malignant']
 #new image size after resizing
 IMG_SIZE = 224
-IMG_AMOUNT = 23900
+IMG_AMOUNT = 13785
 
 training_data = []
 
@@ -27,16 +28,21 @@ def createTrainingData():
         img_array = cv2.imread(os.path.join(DATADIR,image))
         new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
         training_data.append([new_array,getLabel(image)])
-        #if len(training_data) >= IMG_AMOUNT:
-         #   break
+        if len(training_data) >= IMG_AMOUNT:
+            break
 
 # This needs to change to work with all data descriptions
 def getLabel(fileName):
     data = openJsonFile(os.path.join(DESCDIR,fileName[:-5]))
-    if data['meta']['clinical']['benign_malignant'] == 'benign':
-        return 0
-    else:
-        return 1
+    if 'meta' in data:
+        if 'clinical' in data['meta']:
+            if 'benign_malignant' in data['meta']['clinical']:
+                if data['meta']['clinical']['benign_malignant'] == 'benign':
+                    return 0
+                else:
+                    return 1
+            else:
+                raise ValueError("No benign bool given in data given at file: " + file)
 
 def openJsonFile(fileName):
     with open(fileName) as f:
