49c8fb50165ccd39389581fc0e509923c31f3c01
Garrett <gmrybarczyk@gmail.com>
2019-02-28 21:21:42 -0700

    Make unit test file for makeDataset

---
+++ makeDatasetTest.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestMakeDatasetMethods(unittest.TestCase):
+
+    def test_createTrainingData(self):
+
+
+    def test_getLabel(self):
+
+
+    def test_openJsonFile(self):
+
+if __name__ == '__main__':
+    unittest.main()
