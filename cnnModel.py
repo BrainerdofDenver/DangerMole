fecc3a9e676b501122e6bda17c1e20e866e90dcf
Garrett <gmrybarczyk@gmail.com>
2019-02-28 19:28:31 -0700

    Changed cnnModel. Don't know what I did but it works pretty good now.

--- cnnModel.py
+++ cnnModel.py
@@ -1,5 +1,5 @@
 '''
-Base cnn model. Currently doesn't work acc and lose don't change at all
+Base cnn model.
 '''
 import tensorflow as tf
 from tensorflow.keras.models import Sequential, model_from_json
@@ -8,51 +8,66 @@ import numpy as np
 import matplotlib.pyplot as plt
 from random import shuffle
 from sklearn.metrics import roc_curve,roc_auc_score,classification_report
+from sklearn.model_selection import train_test_split
+import random
+import gc
 
 # Amount of test data
 TEST_PERCENT = 0.2
 
-X = np.load('X_data.npy')
-y = np.load('y_data.npy')
+X_load = np.load('X_data.npy')
+y_load = np.load('y_data.npy')
+benign = []
+malignant = []
 
-# takes a sections of the dataset to train on, otherwise too big to fit into memory
-X = X[3000:5000]
-y = y[3000:5000]
+for i in range(len(X_load)):
+    if y_load[i] == 0:
+        benign.append([X_load[i],y_load[i]])
+    else:
+        malignant.append([X_load[i],y_load[i]])
+print(type(benign))
+print(len(malignant))
 
-X = X/255.0
+# Don't know if this actually does anything but im leaving it here for now
+gc.collect()
 
-# Partition for test and train data
-howMany = int(round(1 - TEST_PERCENT * len(X)))
+X= []
+y= []
 
-X_test = X[howMany:len(X)]
-X = X[0:howMany]
+for i in range(2000):
+    malignant.append(benign[i])
+print(type(malignant))
 
-# Was confused about y so I printed them out. Saw that there are much more benign than malignant
-'''for label in y:
-    print(label)
-'''
-y_test = y[howMany:len(y)]
-y = y[0:howMany]
-print(y_test)
+for feature,label in malignant:
+    X.append(feature)
+    y.append(label)
+
+print(len(X))
+X= np.asarray(X)
+y= np.asarray(y)
+X= X/255.0
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=2)
 
 model = Sequential()
-model.add(Conv2D(64,(3,3),activation='sigmoid', input_shape = (224,224,3)))
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(32,(3,3),activation='relu', input_shape = (224,224,3),data_format='channels_last'))
+model.add(MaxPooling2D(pool_size=(2,2),strides=2))
 
-model.add(Conv2D(32,(3,3),activation='sigmoid'))
+model.add(Conv2D(64,(3,3),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Flatten())
+model.add(Dense(64,activation='relu'))
+model.add(Dropout(0.25))
+
 model.add(Dense(1,activation='sigmoid'))
-'''
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-'''
+
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(X,y,batch_size = 64,epochs=2,shuffle=True,validation_split=0.1)
+model.fit(X_train,y_train,batch_size = 64,epochs=5,shuffle=True,validation_split=0.1)
+
 scores = model.evaluate(X_test,y_test)
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
 
@@ -67,12 +82,15 @@ print("saved model")
 # Just doing my own little test to make sure im not crazy
 predictions = model.predict(X_test)
 i = 0
-fpr = 0
-tpr = 0
 for prediction in predictions:
     print(prediction)
     print(y_test[i])
     i += 1
+
+above_threshold_indices = predictions > 0.5
+below_threshold_indices = predictions < 0.5
+predictions[above_threshold_indices] = 1
+predictions[below_threshold_indices] = 0
 
 # most of this block was taken from https://towardsdatascience.com/building-a-logistic-regression-in-python-step-by-step-becd4d56c9c8
 logit_roc_auc = roc_auc_score(y_test, predictions)
--- dataParser.py
+++ dataParser.py
@@ -7,6 +7,9 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
+from matplotlib import pyplot as plt
+from PIL import Image as Img
+
 
 baseAddrDescriptions = 'E:/\school/tensorflowprc/moleDataSet/Data/Descriptions/'
 baseAddrImages = 'E:/\school/tensorflowprc/moleDataSet/Data/Images/'
@@ -23,9 +26,7 @@ def getLabels(numLabels,fileNames):
     labels = []
     for i in range(numLabels):
         address = baseAddrDescriptions + fileNames[i][:-5]
-        #print(address)
         data = openJsonFile(address)
-        #print(data['meta']['clinical']['benign_malignant'])
         if data['meta']['clinical']['benign_malignant'] == 'benign':
             labels.append(0)
         else:
@@ -35,52 +36,20 @@ def getLabels(numLabels,fileNames):
 def openJsonFile(fileName):
     with open(fileName) as f:
         data = json.load(f)
-        #print(data)
         return data
-'''
-def load_image(addr):
-    img = cv2.imread(addr)
-    if img is None:
-        return None
-    img = cv2.resize(img, (224,224), interpolation=cv2.INTER_CUBIC)
-    return img
-'''
+
 def createDataset(fileNames):
     labels = getLabels(len(fileNames),fileNames)
     print(fileNames)
     print(labels)
+
     def _parseFunction(fileName, label):
         image_string = tf.read_file(baseAddrImages + fileName)
         image_decoded = tf.image.decode_jpeg(image_string, channels=3)
         image_resized = tf.image.resize_images(image_decoded,[224,224])
-        image_resized /= 255.0
         return image_resized, label
+
     dataset = tf.data.Dataset.from_tensor_slices((tf.constant(fileNames),tf.constant(labels)))
     dataset = dataset.map(_parseFunction)
     return dataset
 
-def main():
-    fileNames = createFileNames(10)
-    dataset = createDataset(fileNames)
-
-    train = dataset.take(5)
-    trainIterator = train.make_one_shot_iterator()
-
-    print(dataset.output_types)
-    
-    model = keras.Sequential()
-    model.add(Conv2D(32,
-                     kernel_size=(3,3),
-                     activation='relu'),
-                     )
-
-    model.compile(optimizer='adam',
-        loss='sparse_categorical_crossentropy',
-        metrics=['accuracy'])
-
-    model.fit(x=trainIterator
-            ,epochs=5,
-            steps_per_epoch=30)
-
-if __name__ == '__main__':
-    main()
