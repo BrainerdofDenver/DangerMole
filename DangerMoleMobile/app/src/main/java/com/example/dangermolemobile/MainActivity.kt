5b424ee0f07fb1bb9e41f9d3694e5630fce07c7c
Alex Parmentier <alexparm@gmail.com>
2019-03-03 14:59:41 -0700

    used safer code to implement camera, if an error occurs, a toast will popup informing the user, rather then the program crashing from it

--- DangerMoleMobile/app/src/main/java/com/example/dangermolemobile/MainActivity.kt
+++ DangerMoleMobile/app/src/main/java/com/example/dangermolemobile/MainActivity.kt
@@ -17,32 +17,38 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
-    val cameraRequestCode = 0
+    val CAM_REQUEST_CODE = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-
-
         take_pic_button.setOnClickListener {
             val camIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             if(camIntent.resolveActivity(packageManager) != null) {
-                startActivityForResult(camIntent, cameraRequestCode)
+                startActivityForResult(camIntent, CAM_REQUEST_CODE)
             }
         }
     }
 
-    //Copying tutorial for initial functionality: https://www.youtube.com/watch?v=ondCeqlAwEI
-
-
+    //Code based on tutorial for initial functionality: https://www.youtube.com/watch?v=5wbeWN4hQt0
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        val bitmap = data?.extras?.get("data") as Bitmap
-        camView.setImageBitmap(bitmap)
-    }
 
+        when(requestCode){
+            CAM_REQUEST_CODE -> {
+                if (resultCode == Activity.RESULT_OK && data != null){
+                    val bitmap: Bitmap = data.extras.get("data") as Bitmap
+                    camView.setImageBitmap(bitmap)
+                }
+            }
+            else -> {
+                Toast.makeText(this, "Unrecognized request code", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
