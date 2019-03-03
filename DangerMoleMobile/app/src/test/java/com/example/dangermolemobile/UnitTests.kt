be34f5d11feccc3a96b376e0affbdf9a859d572b
Alex Parmentier <37607570+alexparm4@users.noreply.github.com>
2019-03-03 16:45:57 -0700

    Created UnitTests.kt file to hold unit tests, and imported libraries needed from Espresso

---
+++ DangerMoleMobile/app/src/test/java/com/example/dangermolemobile/UnitTests.kt
@@ -0,0 +1,30 @@
+package com.example.dangermolemobile
+
+import android.content.Intent
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.espresso.assertion.ViewAssertions.*
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class UnitTests {
+
+    @Test
+    fun camButtonShouldReturnIntent(){
+        //val expectedIntent = Intent
+    }
+
+    //https://cate.blog/2016/04/28/testing-intents-on-android-like-stabbing-yourself-in-the-eye-with-a-blunt-implement/
+    @Test
+    fun testLaunchActivity() {
+//        onView(withId(R.id.home_camera_button)).check(matches(withText("Camera")))
+//        onView(withId(R.id.home_gallery_button)).check(matches(withText("Gallery")))
+//        onView(withId(R.id.home_inspire_button)).check(matches(withText("Inspire")))
+    }
+}
