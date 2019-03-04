5b3892771d0ed5d13aa4349605e7c348e09427eb
Alex Parmentier <alexparm@gmail.com>
2019-03-03 17:04:25 -0700

    Cleaned up UnitTests file, and writing camButtonShouldReturnIntent() method, not yet complete, 4 lines done so far

--- DangerMoleMobile/app/src/test/java/com/example/dangermolemobile/UnitTests.kt
+++ DangerMoleMobile/app/src/test/java/com/example/dangermolemobile/UnitTests.kt
@@ -1,30 +1,29 @@
 package com.example.dangermolemobile
 
+import android.app.Activity
+import android.app.Instrumentation
 import android.content.Intent
+import android.net.Uri
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.espresso.assertion.ViewAssertions.*
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class UnitTests {
 
     @Test
     fun camButtonShouldReturnIntent(){
-        //val expectedIntent = Intent
+        val uri = Uri.parse("data")
+        var resultData = Intent()
+        resultData.data = uri
+        val result = Instrumentation.ActivityResult(Activity.RESULT_OK, resultData)
     }
 
-    //https://cate.blog/2016/04/28/testing-intents-on-android-like-stabbing-yourself-in-the-eye-with-a-blunt-implement/
+    //Found from this site, which helped with espresso imports https://cate.blog/2016/04/28/testing-intents-on-android-like-stabbing-yourself-in-the-eye-with-a-blunt-implement/
     @Test
     fun testLaunchActivity() {
-//        onView(withId(R.id.home_camera_button)).check(matches(withText("Camera")))
-//        onView(withId(R.id.home_gallery_button)).check(matches(withText("Gallery")))
-//        onView(withId(R.id.home_inspire_button)).check(matches(withText("Inspire")))
+        onView(withId(R.id.take_pic_button)).check(matches(withText("SNAP")))
+        onView(withId(R.id.probabilityView)).check(matches(withText("Probability:  ___%")))
+        onView(withId(R.id.accuracyView)).check(matches(withText("Probability:  ___%")))
     }
 }
