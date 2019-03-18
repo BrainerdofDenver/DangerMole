829a861606dc13965ec5144d0e009b6c03cb326b
Alex Parmentier <alexparm@gmail.com>
2019-03-18 13:15:45 -0600

    Renamed MainActivity to CameraActivity (better descriptor)

--- DangerMoleMobile/app/src/main/java/com/example/dangermolemobile/CameraActivity.kt
+++ DangerMoleMobile/app/src/main/java/com/example/dangermolemobile/CameraActivity.kt
@@ -16,7 +16,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class CameraActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     val CAM_REQUEST_CODE = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
