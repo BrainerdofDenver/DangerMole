edf65b590068895900174bbe3eb19d2796107c43
Alex Parmentier <alexparm@gmail.com>
2019-03-18 14:22:16 -0600

    Added variables to use for saving images to external memory. Set filename for each image to be saved as current date/time.

--- DangerMoleMobile/app/src/main/java/com/example/dangermolemobile/CameraListener.kt
+++ DangerMoleMobile/app/src/main/java/com/example/dangermolemobile/CameraListener.kt
@@ -4,8 +4,15 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.provider.MediaStore
+import java.io.File
+import java.time.LocalDateTime
+import java.util.*
 
 class CameraListener {
+    val APP_TAG = "DangerMole Mobile"
+    val CAM_REQUEST_CODE = 0
+    var photoFileName = Date(System.currentTimeMillis())
+    lateinit var photoFile: File
 
     fun camIntentSender(requestCode: Int, mContext: Context, mActivity: Activity){
         val camIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
