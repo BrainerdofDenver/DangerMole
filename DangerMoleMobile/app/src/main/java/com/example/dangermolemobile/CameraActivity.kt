7a6e3630f4f464be3f7caaacc2b3cb8d7052d62c
Alex Parmentier <alexparm@gmail.com>
2019-03-20 19:29:37 -0600

    Saving Images works, all methods are currently in CameraActivity()

--- DangerMoleMobile/app/src/main/java/com/example/dangermolemobile/CameraActivity.kt
+++ DangerMoleMobile/app/src/main/java/com/example/dangermolemobile/CameraActivity.kt
@@ -3,21 +3,32 @@ package com.example.dangermolemobile
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.ThumbnailUtils
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.provider.MediaStore
 import android.support.design.widget.NavigationView
+import android.support.v4.content.FileProvider
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import java.io.File
+import java.io.IOException
+import java.util.*
 
 class CameraActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     val CAM_REQUEST_CODE = 0
+    var currentPhotoPath = ""
+    val REQUEST_TAKE_PHOTO = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,31 +45,28 @@ class CameraActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
 
         //Camera Button Implementation
         take_pic_button.setOnClickListener {
-            CameraListener().camIntentSender(CAM_REQUEST_CODE,this, this)
+            //CameraListener().camIntentSender(CAM_REQUEST_CODE,this, this)
+            dispatchTakePictureIntent()
         }
 
-        //This needs to be removed, only implemented for testing
-        val str = Environment.getExternalStorageDirectory().toString() + "/"+"testFile"
-        camView.setOnClickListener {
-            Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
-        }
     }
 
     //Code based on tutorial for initial functionality: https://www.youtube.com/watch?v=5wbeWN4hQt0
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        when (requestCode) {
-            CAM_REQUEST_CODE -> {
-                if (resultCode == Activity.RESULT_OK && data != null) {
-                    val bitmap: Bitmap = data.extras.get("data") as Bitmap
-                    camView.setImageBitmap(bitmap)
-                }
-            }
-            else -> {
-                Toast.makeText(this, "Unrecognized request code", Toast.LENGTH_SHORT).show()
-            }
-        }
+//        when (requestCode) {
+////            CAM_REQUEST_CODE -> {
+////                if (resultCode == Activity.RESULT_OK && data != null) {
+////                    val bitmap: Bitmap = data.extras.get("data") as Bitmap
+////                    camView.setImageBitmap(bitmap)
+////                }
+////            }
+////            else -> {
+////                Toast.makeText(this, "Unrecognized request code", Toast.LENGTH_SHORT).show()
+////            }
+////        }
+        loadPicToPreview()
     }
 
     //Part of Navigation Drawer
@@ -110,5 +118,70 @@ class CameraActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
         }
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private fun dateTimeFormatter(): String{
+        var str = Date(System.currentTimeMillis()).toString()
+        return str.replace(" ", "")
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        val root = File(Environment.getExternalStorageDirectory().toString() + File.separator + "DangerMole" + File.separator)
+        root.mkdirs()
+        return File.createTempFile(
+            dateTimeFormatter(), ".png", root
+        ).apply {
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    Toast.makeText(this, "Error occurred creating file. Please try again.",
+                        Toast.LENGTH_SHORT).show()
+                    val folder_main = "DangerMole"
+
+                    val f = File(Environment.getExternalStorageDirectory(), folder_main)
+                    if (!f.exists()) {
+                        f.mkdirs()
+                    }
+
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.dangermolemobile.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                }
+            }
+        }
+    }
+
+
+    private fun loadPicToPreview(){
+        //https://stackoverflow.com/questions/6908604/android-crop-center-of-bitmap
+        val bm = BitmapFactory.decodeFile(currentPhotoPath)
+        val imgView: ImageView = findViewById(R.id.camView)
+
+        val dimension = getSquareCropDimensionForBitmap(bm)
+
+        val returnedBitMap = ThumbnailUtils.extractThumbnail(bm, dimension, dimension)
+
+        imgView.setImageBitmap(returnedBitMap)
+    }
+
+    private fun getSquareCropDimensionForBitmap(bitmap: Bitmap): Int {
+        //use the smallest dimension of the image to crop to
+        return Math.min(bitmap.width, bitmap.height)
     }
 }
