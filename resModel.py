834b658f8b635b8cf44dc163dbdc16a8473628e1
Garrett <gmrybarczyk@gmail.com>
2019-03-30 21:07:15 -0600

    clean up and update to use modelUtils

--- resModel.py
+++ resModel.py
@@ -7,13 +7,9 @@ from keras.layers import Dense,Dropout,Activation,Flatten,Conv2D,MaxPooling2D,In
 import numpy as np
 import matplotlib.pyplot as plt
 from random import shuffle
-import cv2
-from sklearn.metrics import roc_curve,roc_auc_score,classification_report
-from sklearn.model_selection import train_test_split
 from sklearn.utils import class_weight
 import random
 import os
-#from resnet50 import ResNet50
 from keras.applications.resnet50 import preprocess_input,ResNet50
 from modelUtils import generator,openJsonFile,getLabel
 import gc
@@ -36,57 +32,14 @@ for file_name in X_file_names:
 for file_name in y_file_names:
     y_file_names_full.append(os.path.join(DATADESC, file_name))
 
-print(X_file_names[0])
-print(X_file_names_full[0])
 X_file_names_full = np.array(X_file_names_full)
 y_file_names_full = np.array(y_file_names_full)
 ytest = [getLabel(y_file_names_full[i]) for i in range(0,len(y_file_names_full))]
-print(ytest[0])
-print(len(X_file_names_full))
+
 steps = np.ceil(len(X_file_names_full) / batch_size)
-print(steps)
 
-'''
-X_load = np.load('X_data.npy')
-y_load = np.load('y_data.npy')
-benign = []
-malignant = []
-
-for i in range(len(X_load)):
-    if y_load[i] == 0:
-        benign.append([X_load[i],y_load[i]])
-    else:
-        malignant.append([X_load[i],y_load[i]])
-print(type(benign))
-print(len(malignant))
-
-# Don't know if this actually does anything but im leaving it here for now
-gc.collect()
-del X_load
-del y_load
-
-X= []
-y= []
-
-for i in range(1000):
-    malignant.append(benign[i])
-print(type(malignant))
-
-for feature,label in malignant:
-    X.append(feature)
-    y.append(label)
-
-X= np.asarray(X)
-y= np.asarray(y)
-X= X/255.0
-X = preprocess_input(X)
-
-print(X.shape)
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=2)
-'''
-#class_weight = {0: 25.,1: 1.}
 class_weights = class_weight.compute_class_weight('balanced',np.unique(ytest),ytest)
-print(class_weights)
+
 image_input = Input(shape=(224,224,3))
 
 model = ResNet50(include_top=True, weights='imagenet',
@@ -108,10 +61,9 @@ custom_resnet_model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-#custom_resnet_model.fit(X_train,y_train,batch_size = 32,epochs=15,shuffle=True,validation_split=0.1)
 custom_resnet_model.fit_generator(generator(X_file_names_full, y_file_names_full, batch_size),
                     steps_per_epoch=steps,
-                    epochs=1,
+                    epochs=5,
                     class_weight=class_weights)
 
 
@@ -125,15 +77,7 @@ with open("resModel.json", "w") as json_file:
 
 custom_resnet_model.save_weights("resModel.h5")
 print("saved model")
-
-# Just doing my own little test to make sure im not crazy
-predictions = custom_resnet_model.predict(X_test)
-i = 0
-for prediction in predictions:
-    print(prediction)
-    print(y_test[i])
-    i += 1
-
+'''
 above_threshold_indices = predictions > 0.5
 below_threshold_indices = predictions < 0.5
 predictions[above_threshold_indices] = 1
@@ -155,3 +99,4 @@ plt.savefig('Log_ROC')
 plt.show()
 
 print(classification_report(y_test,predictions))
+'''
