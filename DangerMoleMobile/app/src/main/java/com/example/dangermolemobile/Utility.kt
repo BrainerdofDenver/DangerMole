c36dca1a30b01256d72f7f23216fcce7d80f54fb
Alex Parmentier <alexparm@gmail.com>
2019-04-10 16:07:40 -0600

    Explicitly asks for permissions needed for basic operation (Camera and Storage). If permissions are denied, then every time the Snap (Camera) button is clicked, the permissions are requested again.

--- DangerMoleMobile/app/src/main/java/com/example/dangermolemobile/Utility.kt
+++ DangerMoleMobile/app/src/main/java/com/example/dangermolemobile/Utility.kt
@@ -1,10 +1,20 @@
 package com.example.dangermolemobile
 
+import android.Manifest
 import android.content.Context
 import android.graphics.Bitmap
 import android.widget.Toast
+import android.content.pm.PackageManager
+import android.support.v4.content.ContextCompat
+import android.app.Activity
+import android.os.Build
+import android.support.v4.app.ActivityCompat
 
 class Utility {
+    private val REQUEST_WRITE_STORAGE_REQUEST_CODE = 104
+    private val REQUEST_READ_STORAGE_REQUEST_CODE = 105
+    private val CAMERA_REQUEST_CODE = 298
+
     fun toastCreator(messageToDisplay: String, mContext: Context){
         Toast.makeText(mContext, messageToDisplay, Toast.LENGTH_SHORT).show()
     }
@@ -13,4 +23,16 @@ class Utility {
         //use the smallest dimension of the image to crop to
         return Math.min(bitmap.width, bitmap.height)
     }
+
+    fun requestCameraAndStoragePermissions(mActivity: Activity, mContext: Context) {
+        if (ContextCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(mContext, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(mContext, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(mActivity,arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA), REQUEST_WRITE_STORAGE_REQUEST_CODE )
+        }
+
+    }
+
 }
