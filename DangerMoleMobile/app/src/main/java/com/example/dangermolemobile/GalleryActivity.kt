8b8a330b02cd704ea87829290478d2cc95067be5
Alex Parmentier <alexparm@gmail.com>
2019-05-01 15:38:40 -0600

    PairedProgramming Alex + Andrew : Adding listview_item.xml resource for gallery listview and added filereading methods to GalleryActivity

--- DangerMoleMobile/app/src/main/java/com/example/dangermolemobile/GalleryActivity.kt
+++ DangerMoleMobile/app/src/main/java/com/example/dangermolemobile/GalleryActivity.kt
@@ -7,8 +7,14 @@ import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.drawer_layout_gallery.*
+import java.io.File
 
 class GalleryActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -17,12 +23,50 @@ class GalleryActivity : AppCompatActivity(), NavigationView.OnNavigationItemSele
         setContentView(R.layout.drawer_layout_gallery)
         setSupportActionBar(toolbar)
 
+        val filePath = this.filesDir.toString()
+
+        //var array = arrayOf("Melbourne", "Vienna", "Vancouver", "Toronto", "Calgary", "Adelaide", "Perth", "Auckland", "Helsinki", "Hamburg", "Munich", "New York", "Sydney", "Paris", "Cape Town", "Barcelona", "London", "Bangkok")
+        var listToDisplay = displayListCleaner(populateArrayFromFile(filePath + "/SavedData.txt"))
+
         val toggle = ActionBarDrawerToggle(
             this, drawer_layout_gallery, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
         drawer_layout_gallery.addDrawerListener(toggle)
         toggle.syncState()
         nav_view_gallery.setNavigationItemSelectedListener(this)
+
+        val adapter = ArrayAdapter(this,
+            R.layout.listview_item, listToDisplay)
+
+        val listView: ListView = findViewById(R.id.gallery_list_view)
+        listView.setAdapter(adapter)
+
+        listView.onItemClickListener = object : AdapterView.OnItemClickListener {
+
+            override fun onItemClick(parent: AdapterView<*>, view: View,
+                                     position: Int, id: Long) {
+
+                // value of item that is clicked
+                val itemValue = listView.getItemAtPosition(position) as String
+
+                // Toast the values
+                Toast.makeText(applicationContext,
+                    "Position :$position\nItem Value : $itemValue", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+
+    }
+
+    private fun displayListCleaner(list: Array<String>): ArrayList<String>{
+        var newList = ArrayList<String>()
+        for (item in list){
+            if (item != ""){
+                newList.add(item)
+            }
+        }
+        return newList
+        //var newList: ArrayList<String> = list.removeAt(0)
     }
 
     //Part of Navigation Drawer
@@ -50,6 +94,14 @@ class GalleryActivity : AppCompatActivity(), NavigationView.OnNavigationItemSele
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         NavigationHandler().NavigationOnClickListener(this, this, item)
         return true
+    }
+
+    private fun readFileAsTextUsingInputStream(fileName: String)
+            = File(fileName).inputStream().readBytes().toString(Charsets.UTF_8)
+
+    fun populateArrayFromFile(filePath: String):Array<String>{
+        var str = readFileAsTextUsingInputStream(filePath)
+        return str.split("\n").toTypedArray()
     }
 }
 
